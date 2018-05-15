1f0fdabf8da2cca08e35c1d6e10ade1ad237633b
Kuixi Song <krayc425@gmail.com>
2018-05-15 08:15:14 +0800

    Modify AppiumDemo

--- AppiumDemo/src/com/glen/demo/ContactsTest.java
+++ AppiumDemo/src/com/glen/demo/ContactsTest.java
@@ -20,21 +20,31 @@ public class ContactsTest {
 
     @Before
     public void setUp() throws Exception {
-        //设置 apk 的路径
+        // 设置 apk 的路径
         File classpathRoot = new File(System.getProperty("user.dir"));
         File appDir = new File(classpathRoot, "apps");
         File app = new File(appDir, "ContactManager.apk");
-        //设置自动化相关参数
+        // 设置自动化相关参数
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "Android Emulator");
-        //设置安卓系统版本 capabilities.setCapability("platformVersion", "4.3"); //设置 apk 路径
+
+        // 设置安卓系统版本
+
+        // 坚果手机
+        capabilities.setCapability("deviceName", "a4dc271");
+        capabilities.setCapability("platformVersion", "5.1.1");
+
+        // 模拟器
+//        capabilities.setCapability("deviceName", "Android Emulator");
+//        capabilities.setCapability("platformVersion", "4.4.2");
+
+        // 设置 apk 路径
         capabilities.setCapability("app", app.getAbsolutePath());
-        //设置 app 的主包名和主类名
+        // 设置 app 的主包名和主类名
         capabilities.setCapability("appPackage", "com.example.android.contactmanager");
         capabilities.setCapability("appActivity", ".ContactManager");
-        //初始化
+        // 初始化
         driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
@@ -42,10 +52,18 @@ public class ContactsTest {
     public void addContact() {
         WebElement el = driver.findElementByAccessibilityId("Add Contact");
         el.click();
-        List<WebElement> textFieldsList =
-                driver.findElementsByClassName("android.widget.EditText");
-        textFieldsList.get(0).sendKeys("Some Name");
-        textFieldsList.get(2).sendKeys("Some@example.com");
+
+        WebElement name = driver.findElementById("com.example.android.contactmanager:id/contactNameEditText");
+        WebElement phone = driver.findElementById("com.example.android.contactmanager:id/contactPhoneEditText");
+        WebElement email = driver.findElementById("com.example.android.contactmanager:id/contactEmailEditText");
+
+        name.click();
+        name.sendKeys("Some Name");
+        phone.click();
+        phone.sendKeys("18795963603");
+        email.click();
+        email.sendKeys("Some@example.com");
+
         driver.swipe(100, 500, 100, 100, 2);
         driver.findElementByAccessibilityId("Save").click();
     }
--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Lab for Software Testing and Quality.
 # 参考链接
 
 * [EvoSuite](http://www.evosuite.org)
+* [Appium](http://appium.io/)
+
 
